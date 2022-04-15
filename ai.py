25170450b4456a112b65605e78a3f3eef0fef08c
Vansh Patpatia <68200019+vansh10patpatia@users.noreply.github.com>
2022-04-15 21:58:51 +0530

    comments added

--- ai.py
+++ ai.py
@@ -5,9 +5,9 @@ df = pd.read_csv(r'./venv/heights.csv')
 Q1 = round(df.height.quantile(0.25))
 Q2 = round(df.height.quantile(0.5))
 Q3 = round(df.height.quantile(0.75))
-IQR = Q3-Q1
-lower_bound = Q1-(1.5*IQR)
-upper_bound = Q3+(1.5*IQR)
+IQR = Q3-Q1 #inter quartile range
+lower_bound = Q1-(1.5*IQR) #lower bound using IQR
+upper_bound = Q3+(1.5*IQR) #upper bound using IQR
 df[(df.height > lower_bound) | (df.height < upper_bound )]
 df_without_outliners = df[(df.height > lower_bound) & (df.height < upper_bound )]
 print(df_without_outliners)
